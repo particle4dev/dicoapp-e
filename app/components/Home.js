398bfba22cbb2e5e1d82f8818f8325d47d8287e3
Hoang Van Nam <particle4dev@gmail.com>
2018-08-19 14:25:25 +0700

    install atlaskit package (#1)
    * package: add atlaskit package
    
    * github: add github template

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import AkAvatar from '@atlaskit/avatar';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
@@ -13,6 +14,7 @@ export default class Home extends Component<Props> {
     return (
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
+        <AkAvatar size="small" />
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>
     );
