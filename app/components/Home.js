55a1ba0c2de77e6b2289768c0c5df5aaebbbe251
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-08-21 22:05:43 +0700

    plugin marketmaker: add ipc control

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,21 +1,39 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { ipcRenderer } from 'electron';
 import AkAvatar from '@atlaskit/avatar';
 import AkButton from '@atlaskit/button';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
+// import getBalance from './get-balance';
+import login from '../login';
+
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  console.log(arg); // prints "pong"
+});
 
 type Props = {};
 
 export default class Home extends Component<Props> {
   props: Props;
 
-  onClick = (evt: SyntheticEvent<HTMLButtonElement>) => {
+  onStartButtonClick = (evt: SyntheticEvent<*>) => {
     evt.preventDefault();
-    alert('1234');
 
-    console.log('1234');
+    ipcRenderer.send('marketmaker:start', 'ping');
+  };
+
+  onStopButtonClick = (evt: SyntheticEvent<*>) => {
+    evt.preventDefault();
+
+    ipcRenderer.send('marketmaker:stop', 'ping');
+  };
+
+  onLoginButtonClick = (evt: SyntheticEvent<*>) => {
+    evt.preventDefault();
+
+    login();
   };
 
   render() {
@@ -24,8 +42,16 @@ export default class Home extends Component<Props> {
         <h2>Home</h2>
         <AkAvatar size="small" />
         <br />
-        <AkButton appearance="primary" onClick={this.onClick}>
-          Button
+        <AkButton appearance="primary" onClick={this.onStartButtonClick}>
+          Start marketmaker
+        </AkButton>
+        <br />
+        <AkButton appearance="warning" onClick={this.onStopButtonClick}>
+          Stop marketmaker
+        </AkButton>
+        <br />
+        <AkButton appearance="warning" onClick={this.onLoginButtonClick}>
+          Login
         </AkButton>
         <br />
         <Link to={routes.COUNTER}>to Counter</Link>
@@ -41,3 +67,15 @@ export default class Home extends Component<Props> {
     );
   }
 }
+
+// TEST
+// setTimeout(() => {
+//   login();
+// }, 5000);
+
+// setTimeout(() => {
+//   getBalance('KMD');
+//   getBalance('GLXT');
+//   getBalance('BTC');
+//   getBalance('LTC');
+// }, 10000);
