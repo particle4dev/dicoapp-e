7fca95811936e2b091948cf8235611a1a9304a9b
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-08-21 16:28:53 +0700

    utils: add BarterDexAPI

--- app/utils/barter-dex-api/index.js
+++ app/utils/barter-dex-api/index.js
@@ -1,9 +1,51 @@
-// const debug = require('debug')('dicoapp:utils:barter-dex-api');
+import FetchService from '../fetch-service';
+import { config } from '../../config/config-default';
+
+const debug = require('debug')('dicoapp:utils:barter-dex-api');
 
 export default class BarterDexAPI {
-  // constructor() {
-  // }
-  // isReady() {
-  //   debug('isReady')
+  constructor(
+    settings = {
+      url: config.barterdex,
+      name: ''
+    }
+  ) {
+    debug(`url = ${settings.url}`);
+    this.fetch = new FetchService({
+      name: settings.name,
+      options: {},
+      base: settings.url
+    });
+  }
+
+  isReady() {
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(async () => {
+        try {
+          debug('isReady');
+          await this.fetch.find({});
+          clearInterval(interval);
+          resolve('OK');
+          // eslint-disable-next-line no-empty
+        } catch (_) {}
+      }, 100);
+
+      setTimeout(() => {
+        clearInterval(interval);
+        reject(new Error('Giving up trying to connect to marketmaker'));
+      }, 10000);
+    });
+  }
+
+  post(data) {
+    return this.fetch.create('', data);
+  }
+
+  create(data) {
+    return this.fetch.create('', data);
+  }
+
+  // get() {
+
   // }
 }
