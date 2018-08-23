8426e2c6902948e7bfa1ae6f9dad2b947374860b
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-08-23 17:02:22 +0700

    reducers: add location reducer

--- app/reducers.js
+++ app/reducers.js
@@ -2,9 +2,9 @@
  * Combine all reducers in this file and export the combined reducers.
  */
 
-// import { fromJS } from 'immutable';
+import { fromJS } from 'immutable';
 import { combineReducers } from 'redux-immutable';
-// import { LOCATION_CHANGE } from 'react-router-redux';
+import { LOCATION_CHANGE } from 'react-router-redux';
 
 // import globalReducer from 'containers/App/reducer';
 // import languageProviderReducer from 'containers/LanguageProvider/reducer';
@@ -18,31 +18,32 @@ import { combineReducers } from 'redux-immutable';
  */
 
 // Initial routing state
-// const routeInitialState = fromJS({
-//   location: null,
-// });
+const routeInitialState = fromJS({
+  location: null
+});
 
 /**
  * Merge route into the global application state
+ * https://github.com/reactjs/react-router-redux/blob/master/src/reducer.js
  */
-// export function routeReducer(state = routeInitialState, action) {
-//   switch (action.type) {
-//     /* istanbul ignore next */
-//     case LOCATION_CHANGE:
-//       return state.merge({
-//         location: action.payload,
-//       });
-//     default:
-//       return state;
-//   }
-// }
+export function routeReducer(state = routeInitialState, action) {
+  switch (action.type) {
+    /* istanbul ignore next */
+    case LOCATION_CHANGE:
+      return state.merge({
+        location: action.payload
+      });
+    default:
+      return state;
+  }
+}
 
 /**
  * Creates the main reducer with the dynamically injected ones
  */
 export default function createReducer(injectedReducers) {
   return combineReducers({
-    // route: routeReducer,
+    route: routeReducer,
     // global: globalReducer,
     // language: languageProviderReducer,
     ...injectedReducers
