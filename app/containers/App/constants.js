4591b21b5b23d55f62f56767760829be97777348
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-08-23 22:17:20 +0700

    app: update

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -1,3 +1,14 @@
-export const LOAD_REPOS = 'dicoapp/App/LOAD_REPOS';
-export const LOAD_REPOS_SUCCESS = 'dicoapp/App/LOAD_REPOS_SUCCESS';
-export const LOAD_REPOS_ERROR = 'dicoapp/App/LOAD_REPOS_ERROR';
+/*
+ * AppConstants
+ * Each action has a corresponding type, which the reducer knows and picks up on.
+ * To avoid weird typos between the reducer and the actions, we save them as
+ * constants here. We prefix them with 'yourproject/YourComponent' so we avoid
+ * reducers accidentally picking up actions they shouldn't.
+ *
+ * Follow this format:
+ * export const YOUR_ACTION_CONSTANT = 'yourproject/YourContainer/YOUR_ACTION_CONSTANT';
+ */
+
+export const LOGIN = 'dicoapp/App/LOGIN';
+export const LOGIN_SUCCESS = 'dicoapp/App/LOGIN_SUCCESS';
+export const LOGIN_ERROR = 'dicoapp/App/LOGIN_ERROR';
--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -1,1 +1,43 @@
-// reducer
+/*
+ * AppReducer
+ *
+ * The reducer takes care of our data. Using actions, we can change our
+ * application state.
+ * To add a new action, add it to the switch statement in the reducer function
+ *
+ * Example:
+ * case YOUR_ACTION_CONSTANT:
+ *   return state.set('yourStateVariable', true);
+ */
+
+import { fromJS } from 'immutable';
+
+import { LOGIN, LOGIN_SUCCESS, LOGIN_ERROR } from './constants';
+
+// The initial state of the App
+const initialState = fromJS({
+  loading: false,
+  error: false,
+  currentUser: null
+});
+
+function appReducer(state = initialState, action) {
+  switch (action.type) {
+    case LOGIN:
+      return state
+        .set('loading', true)
+        .set('error', false)
+        .setIn(['userData', 'repositories'], false);
+    case LOGIN_SUCCESS:
+      return state
+        .setIn(['userData', 'repositories'], action.repos)
+        .set('loading', false)
+        .set('currentUser', action.username);
+    case LOGIN_ERROR:
+      return state.set('error', action.error).set('loading', false);
+    default:
+      return state;
+  }
+}
+
+export default appReducer;
