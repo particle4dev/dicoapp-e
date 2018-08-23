e2b9e9d0aa5c7b585e537b5def2de08141fc125e
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-08-23 16:37:36 +0700

    containers: add constants and actions

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -1,1 +1,3 @@
-// constants
+export const LOAD_REPOS = 'dicoapp/App/LOAD_REPOS';
+export const LOAD_REPOS_SUCCESS = 'dicoapp/App/LOAD_REPOS_SUCCESS';
+export const LOAD_REPOS_ERROR = 'dicoapp/App/LOAD_REPOS_ERROR';
