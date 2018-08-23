b346577bd14e77b97bbf3a2e532535525983bbf3
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-08-23 16:40:23 +0700

    home: update

--- app/containers/HomePage/constants.js
+++ app/containers/HomePage/constants.js
@@ -1,1 +1,2 @@
-// constants
+// eslint-disable-next-line import/prefer-default-export
+export const CHANGE_USERNAME = 'dicoapp/Home/CHANGE_USERNAME';
--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -1,1 +1,9 @@
-// selectors
+import { createSelector } from 'reselect';
+import { initialState } from './reducer';
+
+const selectHome = state => state.get('home', initialState);
+
+const makeSelectUsername = () =>
+  createSelector(selectHome, homeState => homeState.get('username'));
+
+export { selectHome, makeSelectUsername };
