c86460115cbb64566ea4595252e764cad6b28ebf
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-08-23 22:22:19 +0700

    reducers: add global reducer

--- app/reducers.js
+++ app/reducers.js
@@ -6,7 +6,7 @@ import { fromJS } from 'immutable';
 import { combineReducers } from 'redux-immutable';
 import { LOCATION_CHANGE } from 'react-router-redux';
 
-// import globalReducer from 'containers/App/reducer';
+import globalReducer from './containers/App/reducer';
 // import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
 /*
@@ -44,7 +44,7 @@ export function routeReducer(state = routeInitialState, action) {
 export default function createReducer(injectedReducers) {
   return combineReducers({
     route: routeReducer,
-    // global: globalReducer,
+    global: globalReducer,
     // language: languageProviderReducer,
     ...injectedReducers
   });
