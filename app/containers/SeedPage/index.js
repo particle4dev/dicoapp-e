5885ccc17b2f579f5440fcefab5d6d8010dbfe52
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-08-27 16:49:26 +0700

    containers: update SeedPage

--- app/containers/SeedPage/index.js
+++ app/containers/SeedPage/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../../constants/routes.json';
+import { EmptyLayout } from '../Layout';
 
 type Props = {};
 
@@ -11,11 +12,11 @@ export default class SeedPage extends Component<Props> {
   render() {
     // const {} = this.props;
     return (
-      <div>
+      <EmptyLayout>
         SeedPage
         <br />
         <Link to={routes.HOME}>to HomePage</Link>
-      </div>
+      </EmptyLayout>
     );
   }
 }
