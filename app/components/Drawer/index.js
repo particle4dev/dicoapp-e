20c215c28c00146e0ea339699078ddf9d118f76f
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-08-29 23:19:43 +0700

    navigation layout: update

---
+++ app/components/Drawer/index.js
@@ -0,0 +1,141 @@
+// @flow
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import { withRouter } from 'react-router';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Dashboard from '@material-ui/icons/Dashboard';
+import LiveHelpIcon from '@material-ui/icons/LiveHelp';
+import { withStyles } from '@material-ui/core/styles';
+import routes from '../../constants/routes.json';
+
+const debug = require('debug')('dicoapp:components:Drawer');
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9
+    }
+  },
+  docked: {
+    height: '100%'
+  }
+});
+
+type Props = {
+  // eslint-disable-next-line flowtype/no-weak-types
+  classes: Object,
+  // eslint-disable-next-line flowtype/no-weak-types
+  history: Object
+};
+
+type State = {
+  anchor: string
+};
+
+class DICDrawer extends Component<Props, State> {
+  state = {
+    anchor: 'left'
+  };
+
+  gotoWalletPage = () => {
+    const { history } = this.props;
+    history.push(routes.WALLET);
+  };
+
+  gotoBuyPage = () => {
+    const { history } = this.props;
+    history.push(routes.BUY);
+  };
+
+  gotoHelpPage = () => {
+    const { history } = this.props;
+    history.push(routes.HELP);
+  };
+
+  render() {
+    debug(`render`);
+
+    // eslint-disable-next-line react/prop-types
+    const { classes } = this.props;
+    const { anchor } = this.state;
+
+    return (
+      <Drawer
+        variant="permanent"
+        classes={{
+          docked: classes.docked,
+          paper: classNames(classes.drawerPaper, classes.drawerPaperClose)
+        }}
+        anchor={anchor}
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          <ListItem button selected active onClick={this.gotoWalletPage}>
+            <ListItemIcon selected active>
+              <Dashboard />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem button onClick={this.gotoWalletPage}>
+            <ListItemIcon>
+              <AccountBalanceWalletIcon />
+            </ListItemIcon>
+            <ListItemText primary="Wallet" />
+          </ListItem>
+          <ListItem button onClick={this.gotoBuyPage}>
+            <ListItemIcon>
+              <AddShoppingCartIcon />
+            </ListItemIcon>
+            <ListItemText primary="Buy" />
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem button onClick={this.gotoHelpPage}>
+            <ListItemIcon>
+              <LiveHelpIcon />
+            </ListItemIcon>
+            <ListItemText primary="Help" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <PowerSettingsNewIcon />
+            </ListItemIcon>
+            <ListItemText primary="Exit" />
+          </ListItem>
+        </List>
+      </Drawer>
+    );
+  }
+}
+
+DICDrawer.displayName = 'DICDrawer';
+
+export default withRouter(withStyles(styles)(DICDrawer));
--- app/containers/Layout/NavigationLayout.js
+++ app/containers/Layout/NavigationLayout.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import { remote } from 'electron';
 import { setWindowBounds } from 'electron-util';
 import { withStyles } from '@material-ui/core/styles';
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
 
 import { minWindowSize } from '../../config/config-default';
-// import DICDrawer from '../../components/Drawer';
+import DICDrawer from '../../components/Drawer';
 // import routes from '../../constants/routes.json';
 
 const debug = require('debug')('dicoapp:containers:layout:NavigationLayout');
@@ -30,9 +30,12 @@ const setAppWindowBounds = (size = minWindowSize) => {
 
 // const drawerWidth = 240;
 
-// const styles = theme => ({
-const styles = () => ({
+const styles = theme => ({
   root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
     display: 'flex'
   },
 
@@ -43,18 +46,35 @@ const styles = () => ({
 
   flex: {
     flexGrow: 1
+  },
+
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+    marginLeft: 72,
+    marginTop: 64
+  },
+
+  dicoDrawer: {
+    color: 'rgba(0, 0, 0, .87)',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    '-webkit-box-sizing': 'border-box',
+    boxSizing: 'border-box',
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
+    contain: 'strict'
   }
 });
 
-const DICTypography = styled.div`
-  font-family: Roboto, sans-serif;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-`;
-
-const DICContent = styled.div`
-  margin-top: 64px;
-`;
+// const DICTypography = styled.div`
+//   font-family: Roboto, sans-serif;
+//   -moz-osx-font-smoothing: grayscale;
+//   -webkit-font-smoothing: antialiased;
+// `;
 
 class NavigationLayout extends Component {
   constructor(props) {
@@ -69,10 +89,18 @@ class NavigationLayout extends Component {
     const { children, classes } = this.props;
 
     return (
-      <div className={classes.root}>
-        <DICTypography>
-          <header className="mdc-toolbar mdc-toolbar--fixed fl-empty-layout__header">
-            <AppBar position="static">
+      <React.Fragment>
+        {/* <header className="mdc-toolbar mdc-toolbar--fixed fl-empty-layout__header">
+        header 123
+      </header> */}
+        <aside className={classes.dicoDrawer}>
+          <DICDrawer />
+        </aside>
+        <main className={classes.content}>{children}</main>
+        <div className={classes.root}>
+          {/* <DICTypography> */}
+          {/* <header className="mdc-toolbar mdc-toolbar--fixed fl-empty-layout__header"> */}
+          {/* <AppBar position="static">
               <Toolbar>
                 <IconButton
                   className={classes.menuButton}
@@ -90,15 +118,15 @@ class NavigationLayout extends Component {
                 </Typography>
                 <Button color="inherit">Login</Button>
               </Toolbar>
-            </AppBar>
-          </header>
-          {/* <aside className="mdc-drawer mdc-drawer--temporary mdc-drawer--open mdc-drawer--animating">
-            <DICDrawer />
-          </aside> */}
-          <DICContent>{children}</DICContent>
-          <div className="mdc-layout-grid">footer</div>
-        </DICTypography>
-      </div>
+            </AppBar> */}
+          {/* </header> */}
+          {/* <aside className="mdc-drawer mdc-drawer--temporary mdc-drawer--open mdc-drawer--animating"> */}
+          {/* </aside> */}
+
+          {/* </DICTypography> */}
+        </div>
+        <div className="mdc-layout-grid">footer</div>
+      </React.Fragment>
     );
   }
 }
