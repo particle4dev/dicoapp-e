2a297859ac555bcb3edb78189671150dcfae6188
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-09-01 12:12:42 +0700

    saved (+1 squashed commit) Squashed commits: [31c4afc] saved

--- app/config/coins-data.js
+++ app/config/coins-data.js
@@ -8,5 +8,37 @@ module.exports = [
     p2shtype: 5,
     wiftype: 176,
     txfee: 100000
+  },
+  {
+    coin: 'BEER',
+    name: 'Beer',
+    asset: 'BEER',
+    rpcport: 8923,
+    electrumServers: [
+      {
+        host: 'electrum1.cipig.net',
+        port: 10022
+      },
+      {
+        host: 'electrum2.cipig.net',
+        port: 10022
+      }
+    ]
+  },
+  {
+    coin: 'PIZZA',
+    name: 'Pizza',
+    asset: 'PIZZA',
+    rpcport: 11116,
+    electrumServers: [
+      {
+        host: 'electrum1.cipig.net',
+        port: 10024
+      },
+      {
+        host: 'electrum2.cipig.net',
+        port: 10024
+      }
+    ]
   }
 ];
