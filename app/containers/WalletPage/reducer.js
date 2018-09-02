c2cb2453988b5c64985edd3e37f3a89ff096265c
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-09-02 22:57:54 +0700

    saved

--- app/containers/WalletPage/reducer.js
+++ app/containers/WalletPage/reducer.js
@@ -3,12 +3,20 @@ import { fromJS } from 'immutable';
 import {
   LOAD_TRANSACTIONS,
   LOAD_TRANSACTIONS_SUCCESS,
-  LOAD_TRANSACTIONS_ERROR
+  LOAD_TRANSACTIONS_ERROR,
+  LOAD_BALANCE,
+  LOAD_BALANCE_SUCCESS,
+  LOAD_BALANCE_ERROR
 } from './constants';
 
 // The initial state of the App
 const initialState = fromJS({
   transactions: {
+    loading: false,
+    error: false,
+    list: []
+  },
+  balance: {
     loading: false,
     error: false,
     list: []
@@ -29,6 +37,19 @@ function walletReducer(state = initialState, { type, payload, error }) {
       return state
         .setIn(['transactions', 'error'], error)
         .setIn(['transactions', 'loading'], false);
+
+    case LOAD_BALANCE:
+      return state
+        .setIn(['balance', 'loading'], true)
+        .setIn(['balance', 'error'], false);
+    case LOAD_BALANCE_SUCCESS:
+      return state
+        .setIn(['balance', 'loading'], false)
+        .setIn(['balance', 'list'], fromJS(payload.balance));
+    case LOAD_BALANCE_ERROR:
+      return state
+        .setIn(['balance', 'error'], error)
+        .setIn(['balance', 'loading'], false);
     default:
       return state;
   }
