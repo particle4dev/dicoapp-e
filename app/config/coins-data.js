91e18b0c188b284ffcd0703bdcb196b97b9aae98
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-09-02 22:33:34 +0700

    electrum: add COQUI data

--- app/config/coins-data.js
+++ app/config/coins-data.js
@@ -106,5 +106,21 @@ module.exports = [
         port: 60012
       }
     ]
+  },
+  {
+    coin: 'COQUI',
+    name: 'Coqui Cash',
+    asset: 'COQUI',
+    rpcport: 14276,
+    electrumServers: [
+      {
+        host: 'electrum1.cipig.net',
+        port: 10011
+      },
+      {
+        host: 'electrum2.cipig.net',
+        port: 10011
+      }
+    ]
   }
 ];
