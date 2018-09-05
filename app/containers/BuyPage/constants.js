5616041538206494b0696c5a537cc3569e1c7745
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-09-05 15:21:02 +0700

    buy page: init

---
+++ app/containers/BuyPage/constants.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/prefer-default-export
+export const APP_STATE_NAME = 'buy';
+
+export const LOAD_PRICE = 'dicoapp/BuyPage/LOAD_PRICE';
+export const LOAD_PRICE_SUCCESS = 'dicoapp/BuyPage/LOAD_PRICE_SUCCESS';
+export const LOAD_PRICE_ERROR = 'dicoapp/BuyPage/LOAD_PRICE_ERROR';
