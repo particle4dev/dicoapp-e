261c4bf4e4b284a25d1ebd14b2cbbad035a9d921
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-09-08 00:18:03 +0700

    wallet: remove actions

--- app/containers/WalletPage/actions.js
+++ app/containers/WalletPage/actions.js
@@ -4,12 +4,8 @@ import {
   LOAD_TRANSACTIONS,
   LOAD_TRANSACTIONS_SUCCESS,
   LOAD_TRANSACTIONS_ERROR,
-  LOAD_BALANCE,
-  LOAD_BALANCE_SUCCESS,
-  LOAD_BALANCE_ERROR,
   LOAD_WITHDRAW,
   LOAD_WITHDRAW_SUCCESS,
-  LOAD_COIN_BALANCE_SUCCESS,
   LOAD_WITHDRAW_ERROR
 } from './constants';
 
@@ -31,38 +27,6 @@ export function loadTransactionsSuccess(transactions) {
 export function loadTransactionsError(message: string) {
   return {
     type: LOAD_TRANSACTIONS_ERROR,
-    error: {
-      message
-    }
-  };
-}
-
-export function loadBalance() {
-  return {
-    type: LOAD_BALANCE
-  };
-}
-
-export function loadBalanceSuccess() {
-  return {
-    type: LOAD_BALANCE_SUCCESS
-  };
-}
-
-export function loadCoinBalanceSuccess(payload: {
-  address: string,
-  balance: number,
-  coin: string
-}) {
-  return {
-    type: LOAD_COIN_BALANCE_SUCCESS,
-    payload
-  };
-}
-
-export function loadBalanceError(message: string) {
-  return {
-    type: LOAD_BALANCE_ERROR,
     error: {
       message
     }
