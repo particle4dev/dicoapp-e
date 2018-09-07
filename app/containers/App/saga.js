2b56f2afa77a489e4f57be3123c7f4cb4fe64d81
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-09-07 23:46:48 +0700

    app: add saga

--- app/containers/App/saga.js
+++ app/containers/App/saga.js
@@ -1,8 +1,23 @@
 // https://github.com/react-boilerplate/react-boilerplate/issues/1277#issuecomment-263267639
-import { fork, take, put } from 'redux-saga/effects';
+import {
+  fork,
+  take,
+  put,
+  takeLatest,
+  select,
+  call,
+  all
+} from 'redux-saga/effects';
 import { push } from 'react-router-redux';
+import api from '../../utils/barter-dex-api';
 import routes from '../../constants/routes.json';
-import { LOGOUT } from './constants';
+import { LOGOUT, LOAD_BALANCE } from './constants';
+import { makeSelectCurrentUser } from './selectors';
+import {
+  loadBalanceSuccess,
+  loadCoinBalanceSuccess,
+  loadBalanceError
+} from './actions';
 
 const debug = require('debug')('dicoapp:containers:App:saga');
 
@@ -15,6 +30,60 @@ export function* logoutFlow() {
   }
 }
 
+export function* loadCoinBalanceProcess(coin, address, userpass) {
+  try {
+    debug(`loadCoinBalanceProcess running${coin}`);
+    const params = {
+      userpass,
+      coin,
+      address
+    };
+    const data = yield api.getBalance(params);
+    data.address = address;
+    yield put(
+      loadCoinBalanceSuccess({
+        coin,
+        address,
+        balance: Number(data.balance)
+      })
+    );
+    debug(`load balance done ${coin}`, data);
+
+    return {
+      address: data.address,
+      balance: data.balance,
+      coin: data.coin
+    };
+  } catch (err) {
+    debug(`loadCoinBalanceProcess fail ${coin}: ${err.message}`);
+    return false;
+  }
+}
+
+export function* loadBalanceProcess() {
+  try {
+    // load user data
+    const user = yield select(makeSelectCurrentUser());
+    if (!user) {
+      throw new Error('not found user');
+    }
+    const userpass = user.get('userpass');
+    const coins = user.get('coins');
+    const requests = [];
+    for (let i = 0; i < coins.size; i += 1) {
+      const e = coins.get(i);
+      const coin = e.get('coin');
+      const address = e.get('smartaddress');
+      requests.push(call(loadCoinBalanceProcess, coin, address, userpass));
+    }
+    yield all(requests);
+    return yield put(loadBalanceSuccess());
+  } catch (err) {
+    return yield put(loadBalanceError(err.message));
+  }
+}
+
 export default function* root() {
+  yield takeLatest(LOAD_BALANCE, loadBalanceProcess);
   yield fork(logoutFlow);
 }
