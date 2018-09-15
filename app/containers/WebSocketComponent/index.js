4ce4036cfa319531f3f9e68b41c0080f92e48a0f
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-09-16 03:56:41 +0700

    websocket component: add makeSelectIsStartWebsocket

--- app/containers/WebSocketComponent/index.js
+++ app/containers/WebSocketComponent/index.js
@@ -1,20 +1,45 @@
 // @flow
 import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
 import injectReducer from '../../utils/inject-reducer';
 import reducer from './reducer';
 import { APP_STATE_NAME } from './constants';
+import { makeSelectIsStartWebsocket } from './selectors';
 
 const debug = require('debug')('dicoapp:containers:WebSocketComponent');
 
-type Props = {};
+type Props = {
+  isStartWebsocket: boolean
+};
 
 class WebSocket extends React.Component<Props> {
+  componentDidUpdate(prevProps) {
+    // Typical usage (don't forget to compare props):
+    const { isStartWebsocket } = this.props;
+    if (isStartWebsocket !== prevProps.isStartWebsocket) {
+      debug(`isStartWebsocket = ${isStartWebsocket.toString()} do something`);
+    }
+  }
+
   render() {
     debug('render');
     return null;
   }
 }
 
-const withReducer = injectReducer({ key: APP_STATE_NAME, reducer });
+const mapStateToProps = createStructuredSelector({
+  isStartWebsocket: makeSelectIsStartWebsocket()
+});
 
-export default withReducer(WebSocket);
+const withReducer = injectReducer({ key: APP_STATE_NAME, reducer });
+const withConnect = connect(
+  mapStateToProps,
+  null
+);
+
+export default compose(
+  withReducer,
+  withConnect
+)(WebSocket);
