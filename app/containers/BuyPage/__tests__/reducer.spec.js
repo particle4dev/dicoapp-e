189a66e587a125ac21ad6dc30eae348a76c7bdf2
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-09-17 23:07:55 +0700

    buypage: improve test

--- app/containers/BuyPage/__tests__/reducer.spec.js
+++ app/containers/BuyPage/__tests__/reducer.spec.js
@@ -1,11 +1,13 @@
 import buyReducer, { initialState } from '../reducer';
 import { loadPrices } from '../actions';
 
-describe('BuyPage Reducers', () => {
+describe('containers/BuyPage/reducers/initial', () => {
   it('should return the initial state', () => {
     expect(buyReducer(undefined, {})).toEqual(initialState);
   });
+});
 
+describe('containers/BuyPage/reducers/loadPrices', () => {
   it('should handle the loadPrices action correctly', () => {
     const expectedResult = initialState
       .setIn(['prices', 'loading'], true)
